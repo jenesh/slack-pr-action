3a29338339a6a629944e2257ae4938c5011f063b
Jenesh Napit <jeneshnapit@pursuit.org>
2020-07-30 10:45:04 -0400

    Testing success()

--- index.js
+++ index.js
@@ -2,31 +2,114 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-try {
-  const slackHook = `https://hooks.slack.com/services/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`
+const test = () => {
+  try {
+    const slackHook = `https://hooks.slack.com/services/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`
 
-  console.log(`SLACK HOOK TYPE`, typeof slackHook)
-  console.log(`SLACK HOOK`, slackHook)
+    console.log(`SLACK HOOK TYPE`, typeof slackHook)
+    console.log(`SLACK HOOK`, slackHook)
 
-  const jsonData = {
-    text: 'Hello world!'
+    const jsonData = {
+      text: 'Hello world!'
+    }
+
+    const complexMsg = {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg",
+            "alt_text": "alt text for image"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Kin Khao*\n:star::star::star::star: 1638 reviews\n The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft."
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/korel-1YjNtFtJlMTaC26A/o.jpg",
+            "alt_text": "alt text for image"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Ler Ros*\n:star::star::star::star: 2082 reviews\n I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder."
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/DawwNigKJ2ckPeDeDM7jAg/o.jpg",
+            "alt_text": "alt text for image"
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "actions",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Farmhouse",
+                "emoji": true
+              },
+              "value": "click_me_123"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Kin Khao",
+                "emoji": true
+              },
+              "value": "click_me_123"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Ler Ros",
+                "emoji": true
+              },
+              "value": "click_me_123"
+            }
+          ]
+        }
+      ]
+    }
+
+    axios.post(slackHook, complexMsg)
+      .then(data => console.log(`Sucess`, data))
+      .catch(err => console.log(`Error =>`, err))
+
+  } catch (error) {
+    core.setFailed(error.message);
   }
 
-  // const options = {
-  //   method: 'post',
-  //   url: `${slackHook}`,
-  //   data: JSON.stringify(jsonData),
-  //   headers: { 'content-type': 'application/json' }
-  // }
-
-  // axios.post(options)
-  //   .then(data => console.log(`Sucess`))
-  //   .catch(err => console.log(`Error =>`, err))
-
-  axios.post(slackHook, jsonData)
-    .then(data => console.log(`Sucess`, data))
-    .catch(err => console.log(`Error =>`, err))
-  
-} catch (error) {
-  core.setFailed(error.message);
+  setTimeout(() => console.log(`Waiting`), 5000)
+  return true
 }
+
+
+test()
